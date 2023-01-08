828da953ef384cc08943736ac86336966a1662f8
thronestudio <contact@throne.work>
2023-01-08 11:01:51 -0800

    added blog

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,7 @@ function buildStyles() {
 exports.buildStyles = buildStyles;
 
 function watch() {
-    gulp.watch(['./src/css/*.scss', './src/*.scss', './src/css/home/*.scss', './src/css/calendar/*.scss', './src/css/collections/*.scss', './src/css/products/*/*.scss', './src/css/products/*.scss'], buildStyles);
+    gulp.watch(['./src/css/*.scss', './src/*.scss', './src/css/home/*.scss', './src/css/calendar/*.scss', './src/css/collections/*.scss', './src/css/blog/*.scss', './src/css/products/*/*.scss', './src/css/products/*.scss'], buildStyles);
 }
 exports.watch = watch;
 
